bccbca91d4bd3e8e7638fecc2761b5b8eff95510
nonov <noe.viricel@orange.fr>
2015-03-25 08:32:19 +0700

    create repo

---
+++ js/angular.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function(){	
+	$(".profil").hide();
+	
+	// smooth scroll function
+	$('a[href="#secondPage"]').on('click',function(e) {
+		e.preventDefault();
+
+		var target = this.hash,
+		$target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+			$(".profil").fadeIn(1000);
+		});
+	});	
+});		
